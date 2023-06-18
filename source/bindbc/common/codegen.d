4f985295ae4789be07224a6973a6cdd4837822fa
IchorDev <15670465+ichordev@users.noreply.github.com>
2023-06-18 17:24:06 +0700

    BROKEN C++ (member) function autogen (due to allMembers bug)

--- source/bindbc/common/codegen.d
+++ source/bindbc/common/codegen.d
@@ -32,7 +32,7 @@ unittest{
 		version(CppRuntime_Clang)    return true;
 		else version(CppRuntime_Gcc) return true;
 		else return false;
-	}(){
+	}()){
 		static assert("ImGuiListClipper".mangleofCppDefaultCtor() == "_ZN16ImGuiListClipperC1Ev");
 	}else static if((){
 		version(CppRuntime_Microsoft)        return true;
@@ -42,6 +42,28 @@ unittest{
 		static assert("ImGuiListClipper".mangleofCppDefaultCtor() == "??0ImGuiListClipper@@QAE@XZ");
 	}
 }
+
+size_t badHash(string s) nothrow pure @safe{
+	size_t ret = 0;
+	for(size_t i = 0; i < s.length; i += 2){
+		ret += s[i]; //yes, this is a *really* bad hashing algorithm!
+	}
+
+	return ret * s.length;
+}
+
+enum toStrCT = (size_t val) nothrow pure @safe{
+	enum base = 10;
+	
+	if(!val) return "0";
+
+	string ret;
+	while(val > 0){
+		ret = cast(char)('0' + (val % base)) ~ ret;
+		val /= base;
+	}
+	return ret;
+};
 
 enum makeFnBindFns = (bool staticBinding) nothrow pure @safe{
 	string ret = `
@@ -57,11 +79,14 @@ enum makeFnBinds = (string[][] fns) nothrow pure @safe{
 		if(fn.length == 3){ //C
 			makeFnBinds ~= "\n\textern(C) ";
 		}else if(fn.length == 4){ //C++ or Objective-C
-			makeFnBinds ~= "\n\textern("~fn[0]~") ";
-			fn = fn[1..$];
+			makeFnBinds ~= "\n\textern("~fn[3]~") ";
 		}else assert(0, "Wrong number of function items");
 		
-		makeFnBinds ~= fn[0]~" "~fn[1]~"("~fn[2]~");";
+		if(fn[1] == "this"){
+			makeFnBinds ~= "pragma(mangle, mangleofCppDefaultCtor(__traits(identifier, typeof(this)))) this(int _);";
+		}else{
+			makeFnBinds ~= fn[0]~" "~fn[1]~"("~fn[2]~");";
+		}
 	}
 	return [makeFnBinds];`;
 	}else{
@@ -69,31 +94,35 @@ enum makeFnBinds = (string[][] fns) nothrow pure @safe{
 	string[] symbols;
 	foreach(fn; fns){
 		string prefix;
+		string hash;
 		if(fn.length == 3){ //C
 			prefix = "\n\textern(C) ";
+			hash = "";
 		}else if(fn.length == 4){ //C++ or Objective-C
-			prefix = "\n\textern("~fn[0]~") ";
-			fn = fn[1..$];
+			prefix = "\n\textern("~fn[3]~") ";
+			import bindbc.common.codegen: badHash, toStrCT;
+			hash = badHash(fn[2]).toStrCT();
 		}else assert(0, "Wrong number of function items");
 		
+		symbols ~= "_" ~ fn[1] ~ hash;
+
 		if(fn[2].length > 3 && fn[2][$-3..$] == "..."){
-			makeFnBinds ~= prefix~"private "~fn[0]~" function("~fn[2]~") _"~fn[1]~";";
+			makeFnBinds ~= prefix~"package "~fn[0]~" function("~fn[2]~") _"~fn[1]~hash~";";
 			makeFnBinds ~= "\n\talias "~fn[1]~" = _"~fn[1]~";";
 		}else{
-			makeFnBinds ~= prefix~"private "~fn[0]~" function("~fn[2]~") _"~fn[1]~";";
+			makeFnBinds ~= prefix~"package "~fn[0]~" function("~fn[2]~") _"~fn[1]~hash~";";
 			if(fn[1] == "this"){ //constructor
 				if(fn[2] == ""){ //default constructor
-					makeFnBinds ~= prefix~"pragma(mangle, mangleofCppDefaultCtor(__traits(identifier, typeof(this)))) "~fn[1]~"(int _){ _"~fn[1]~"(); }";
+					makeFnBinds ~= prefix~"pragma(mangle, mangleofCppDefaultCtor(__traits(identifier, typeof(this)))) this(int _){ _this"~hash~"(); }";
 				}else{
-					makeFnBinds ~= prefix~fn[1]~"("~fn[2]~"){ _"~fn[1]~"(__traits(parameters)); }";
+					makeFnBinds ~= prefix~"this("~fn[2]~"){ _this"~hash~"(__traits(parameters)); }";
 				}
-			}else if(fn[0] == "void"){
-				makeFnBinds ~= prefix~fn[0]~" "~fn[1]~"("~fn[2]~"){ _"~fn[1]~"(__traits(parameters)); }";
+			}else if(fn[0].length >= 4 && fn[0][$-4..$] == "void"){
+				makeFnBinds ~= prefix~fn[0]~" "~fn[1]~"("~fn[2]~"){ _"~fn[1]~hash~"(__traits(parameters)); }";
 			}else{
-				makeFnBinds ~= prefix~fn[0]~" "~fn[1]~"("~fn[2]~"){ return _"~fn[1]~"(__traits(parameters)); }";
+				makeFnBinds ~= prefix~fn[0]~" "~fn[1]~"("~fn[2]~"){ return _"~fn[1]~hash~"(__traits(parameters)); }";
 			}
 		}
-		symbols ~= fn[1];
 	}
 	return [makeFnBinds] ~ symbols;`;
 	}
@@ -109,7 +138,7 @@ enum joinFnBinds = (string[][] list, bool bindMemberFns=false) nothrow pure @saf
 	}`;
 	}else{
 		ret ~= `
-	string joined = "@nogc nothrow __gshared{";
+	string joined = "\n@nogc nothrow __gshared{";
 	string[] symbols;
 	foreach(item; list){
 		joined ~= item[0];
@@ -123,14 +152,18 @@ enum joinFnBinds = (string[][] list, bool bindMemberFns=false) nothrow pure @saf
 		ret ~= `
 	joined ~= "\n\nimport bindbc.loader: SharedLib, bindSymbol;\nvoid bindModuleSymbols(SharedLib lib) @nogc nothrow{";
 	foreach(symbol; symbols){
-		joined ~= "\n\tlib.bindSymbol(cast(void**)&_"~symbol~", \""~mixin(symbol~".mangleof")~"\");";
+		joined ~= "\n\tlib.bindSymbol(cast(void**)&"~symbol~", "~symbol~".mangleof);";
 	}
 	if(bindMemberFns){
+		joined ~= q{
+		pragma(msg, "MODULE: "~__MODULE__);
 		static foreach(member; __traits(allMembers, mixin(__MODULE__))){
-			static if((__traits(getLinkage, mixin(member)) == "C++" || __traits(getLinkage, mixin(member)) == "Objective-C") && (is(mixin(member) == struct) || is(mixin(member) == class) || is(mixin(member) == struct))){
-				mixin(member,".bindModuleSymbols();");
+			pragma(msg, member);
+			static if( (is(mixin(member) == struct) || is(mixin(member) == class) || is(mixin(member) == struct)) && (__traits(getLinkage, mixin(member)) == "C++" || __traits(getLinkage, mixin(member)) == "Objective-C") ){
+				mixin(member~".bindModuleSymbols();");
 			}
 		}
+		};
 	}
 	joined ~= "\n}";`;
 	}
@@ -140,3 +173,11 @@ enum joinFnBinds = (string[][] list, bool bindMemberFns=false) nothrow pure @saf
 };`;
 	return ret;
 };
+
+enum makeExpandedEnum(Enum) = () nothrow pure @safe{
+	string ret;
+	foreach(member; __traits(allMembers, Enum)){
+		ret ~= "\nenum "~member~" = "~Enum.stringof~"."~member~";";
+	}
+	return ret;
+}();
