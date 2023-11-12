da7e3a63f5c4401ee5dd5f5e85532a109aa2a1ef
zxin <15670465+ichordev@users.noreply.github.com>
2023-11-13 01:42:29 +1000

    Add c_int64/c_uint64 to types

--- source/bindbc/common/types.d
+++ source/bindbc/common/types.d
@@ -10,15 +10,26 @@ version(WebAssembly){
 	alias c_long  = long;
 	alias c_ulong = ulong;
 	
+	alias c_int64 = long;
+	alias c_uint64 = ulong;
+	
 	alias va_list = void*;
 }else{
-	public import core.stdc.config: c_long, c_ulong;
-	public import core.stdc.stdarg: va_list;
+	static import core.stdc.config;
+	alias c_long = core.stdc.config.c_long;
+	alias c_ulong = core.stdc.config.c_ulong;
+	
+	static import core.stdc.stdint;
+	alias c_int64 = core.stdc.stdint.int64_t;
+	alias c_uint64 = core.stdc.stdint.uint64_t;
+	
+	static import core.stdc.stdarg;
+	alias va_list = core.stdc.stdarg.va_list;
 }
 
 static if((){
-	version(Posix)     return true;
-	else version(WASI) return true;
+	version(Posix)            return true;
+	else version(WebAssembly) return true;
 	else return false;
 }()){
 	alias wchar_t = dchar;
